2531b0396f695ba1afe6ce0b67140738889eeccc
Rizky Dwi Saputra <rizkydwisaputrar1@gmail.com>
2021-12-03 16:26:54 -0800

    Initial commit

---
+++ app/src/main/java/com/rizkydwisaputra/praktek1/model/AlphaChar.kt
@@ -0,0 +1,11 @@
+package com.rizkydwisaputra.praktek.model
+
+class AlphaChar {
+    var iconChar:Int?=0
+    var alphaChar:String?=null
+
+    constructor(icon: Int?, alphaChar: String?) {
+        this.iconChar = icon
+        this.alphaChar = alphaChar
+    }
+}
